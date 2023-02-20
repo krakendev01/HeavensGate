09ad3c57e4820660e6dd5d78fc09bd0045d6d9b7
krakendev01 <sahilpatel19189@gmail.com>
2023-02-20 11:40:26 +0530

    web view code

--- app/src/main/java/com/donation/heavensgate/MainActivity.kt
+++ app/src/main/java/com/donation/heavensgate/MainActivity.kt
@@ -2,10 +2,15 @@ package com.donation.heavensgate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var web:WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+             web=findViewById<WebView>(R.id.webview)
+        web.loadUrl("https://www.google.com/")
     }
 }
