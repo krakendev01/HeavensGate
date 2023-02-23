e0e15b7ba209b41995571fa2bbd51c67b5de11e0
krakendev01 <sahilpatel19189@gmail.com>
2023-02-23 19:46:48 +0530

    email

--- app/src/main/java/com/donation/heavensgate/spalsh.kt
+++ app/src/main/java/com/donation/heavensgate/spalsh.kt
@@ -4,26 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
 class spalsh : AppCompatActivity() {
-    lateinit var  imageview:ImageView
+    lateinit var  i_v:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
         setContentView(R.layout.activity_spalsh)
-        window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_FULLSCREEN
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
-        imageview =findViewById(R.id.image)
-        val slidanimation= AnimationUtils.loadAnimation(this, androidx.appcompat.R.anim.abc_fade_in)
-        imageview.startAnimation(slidanimation)
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        //Normal Handler is deprecated , so we have to change the code little bit
 
-        Handler().postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
             finish()
-        },2500)
-    }
-}
+        }, 3000) // 3000 is the delayed time in milliseconds.
+
+
+        }
+
+
+
+
+        }
+
