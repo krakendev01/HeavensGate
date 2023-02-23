2ec643d367184f38d10f7b12e8ea4fef17501957
aamirbhai2003 <aamirbhai2003@gmail.com>
2023-02-23 12:35:03 +0530

    aamir commit

--- app/src/main/java/com/donation/heavensgate/models/loginmodel.kt
+++ app/src/main/java/com/donation/heavensgate/models/loginmodel.kt
@@ -2,8 +2,19 @@ package com.donation.heavensgate.models
 
 class loginmodel {
 
+
     fun add(a:Int, b:Int): Int {
-       val c= a + b
-       return c
+        val c= a + b
+        return c
     }
+
+    fun multi(a:Int, b:Int): Int {
+        val c= a + b
+        return c
+    }
+    fun sub(a:Int, b:Int): Int {
+        val c= a + b
+        return c
+    }
+
 }
