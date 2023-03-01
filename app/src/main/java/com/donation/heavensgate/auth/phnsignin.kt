68a80bc81f78bbe47e9de0ce8c9b82cfbeb3850d
krakendev01 <sahilpatel19189@gmail.com>
2023-03-01 12:20:26 +0530

    design

---
+++ app/src/main/java/com/donation/heavensgate/auth/phnsignin.kt
@@ -0,0 +1,12 @@
+package com.donation.heavensgate.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.donation.heavensgate.R
+
+class phnsignin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phnsignin)
+    }
+}
