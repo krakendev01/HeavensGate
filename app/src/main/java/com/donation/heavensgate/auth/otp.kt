63de875b635b6295be7ff148c9eeb8ca9e1ae263
krakendev01 <sahilpatel19189@gmail.com>
2023-03-03 18:48:58 +0530

    email

--- app/src/main/java/com/donation/heavensgate/auth/otp.kt
+++ app/src/main/java/com/donation/heavensgate/auth/otp.kt
@@ -3,9 +3,13 @@ package com.donation.heavensgate.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.donation.heavensgate.R
+import com.donation.heavensgate.databinding.ActivityOtpBinding
 
 class otp : AppCompatActivity() {
+
+    lateinit var binding: ActivityOtpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
     }
