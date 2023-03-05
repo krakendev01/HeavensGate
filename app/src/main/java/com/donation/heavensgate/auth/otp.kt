169deb66b531f444359a2bfa3186342366bb9e35
aamirbhai2003 <aamirbhai2003@gmail.com>
2023-03-05 08:49:18 +0530

    aamir commit

--- app/src/main/java/com/donation/heavensgate/auth/otp.kt
+++ app/src/main/java/com/donation/heavensgate/auth/otp.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.donation.heavensgate.R
 
 class otp : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
