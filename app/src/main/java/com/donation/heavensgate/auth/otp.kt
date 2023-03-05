149e5d4aa747f2b194b2ecbb9248dd862e113ae1
aamirbhai2003 <aamirbhai2003@gmail.com>
2023-03-05 08:54:42 +0530

    aamir commit

--- app/src/main/java/com/donation/heavensgate/auth/otp.kt
+++ app/src/main/java/com/donation/heavensgate/auth/otp.kt
@@ -8,5 +8,7 @@ class otp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
+        //created new branch from devs
+
     }
 }
