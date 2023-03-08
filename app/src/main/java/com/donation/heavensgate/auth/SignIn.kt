d7d7ef19f87f8a30419cf004dc5851e9f03cf62b
krakendev01 <sahilpatel19189@gmail.com>
2023-03-09 02:31:18 +0530

    opt

--- app/src/main/java/com/donation/heavensgate/auth/SignIn.kt
+++ app/src/main/java/com/donation/heavensgate/auth/SignIn.kt
@@ -1,24 +1,147 @@
 package com.donation.heavensgate.auth
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
+import com.donation.heavensgate.MainActivity
 import com.donation.heavensgate.R
 import com.donation.heavensgate.databinding.ActivitySigninBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import java.util.concurrent.TimeUnit
 
 class SignIn : AppCompatActivity() {
     lateinit var binding:ActivitySigninBinding
+    private lateinit var auth : FirebaseAuth
+
+    private var storedVerificationId: String? = ""
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySigninBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        auth= FirebaseAuth.getInstance()
+
+        binding.btnotp.setOnClickListener(View.OnClickListener {
+            if (binding.phn.text.isEmpty())
+            {
+                binding.phn.error="Please Enter a phone number"
+            }
+            else
+            {
+                if (Patterns.PHONE.matcher(binding.phn.text).matches())
+                {
+
+                    binding.pb1.visibility=View.VISIBLE
+
+                    val number=binding.phn.text.trim().toString()
+                    val options = PhoneAuthOptions.newBuilder(auth)
+                        .setPhoneNumber("+91$number")       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+                        .build()
+                    PhoneAuthProvider.verifyPhoneNumber(options)
+                }
+            }
+        })
         binding.imageView.setImageResource(R.drawable.logo)
         binding.imageView4.setImageResource(R.drawable.loginillu)
         binding.signup.setOnClickListener(View.OnClickListener {
 
             startActivity(Intent(this@SignIn,donsignup::class.java))
         })
+
+
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:$credential")
+                signInWithPhoneAuthCredential(credential)
+            }
+
+
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e)
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e is FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                }
+
+                // Show a message and update the UI
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:$verificationId")
+
+                // Save verification ID and resending token so we can use them later
+                var storedVerificationId = verificationId
+                var resendToken = token
+                var intent = Intent(this@SignIn,otp::class.java)
+                intent.
+                startActivity(intent)
+                binding.pb1.visibility=View.INVISIBLE
+            }
+        }
     }
+
+
+    // [START sign_in_with_phone]
+    fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    Toast.makeText(this@SignIn,"success",Toast.LENGTH_SHORT).show()
+                    sendtoMain()
+
+                    val user = task.result?.user
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                    // Update UI
+                }
+            }
+    }
+
+
+
+    // [END sign_in_with_phone]
+
+    fun sendtoMain() {
+
+        startActivity(Intent(this,MainActivity::class.java))
+
+    }
+
 }
