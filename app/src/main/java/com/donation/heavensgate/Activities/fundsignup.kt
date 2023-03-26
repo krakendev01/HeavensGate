c5412215abbe914beaeb590d1fed943d2c522108
aamirbhai2003 <aamirbhai2003@gmail.com>
2023-03-26 16:09:24 +0530

    fund design basic

--- app/src/main/java/com/donation/heavensgate/Activities/fundsignup.kt
+++ app/src/main/java/com/donation/heavensgate/Activities/fundsignup.kt
@@ -3,6 +3,7 @@ package com.donation.heavensgate.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.donation.heavensgate.MainActivity
 import com.donation.heavensgate.databinding.ActivityFundsignupBinding
 import com.donation.heavensgate.models.FundUsers
@@ -22,34 +23,7 @@ class fundsignup : AppCompatActivity() {
         auth= FirebaseAuth.getInstance()
         database=FirebaseDatabase.getInstance()
 
-        binding.button2.setOnClickListener{
-            if (binding.Name.text.isEmpty()){
-                binding.Name.error="feild cant be emptey"
-            }
-            if (binding.Email.text.isEmpty()){
-                binding.Email.error="feild cant be emptey"
-            }
-            if (binding.Phone.text.isEmpty()){
-                binding.Phone.error="feild cant be emptey"
-            }
-            if (binding.Pass.text.isEmpty()){
-                binding.Pass.error="feild cant be emptey"
-            }
-            if (binding.ConfirmPass.text.isEmpty())
-                binding.ConfirmPass.error="feild cant be emptey"
 
-            auth.createUserWithEmailAndPassword(binding.Email.text.toString(),binding.Pass.text.toString())
-                .addOnSuccessListener {
-
-                     users= FundUsers(binding.Name.text.toString(),binding.Email.text.toString(),binding.Phone.text.toString(),binding.Pass.text.toString())
-                    database.reference.child("fundusers")
-                        .child(binding.Phone.text.toString())
-                        .setValue(users).addOnSuccessListener {
-                            startActivity(Intent(this@fundsignup,MainActivity::class.java))
-                        }
-                }
-
-        }
 
 
 
---
+++ app/src/main/java/com/donation/heavensgate/Models/AddOrg.kt
@@ -0,0 +1,12 @@
+package com.donation.heavensgate.Models
+
+data class AddOrg(
+    val Name:String? = "",
+    val Email:String? = "",
+    val Phone:String? = "",
+    val Address:String? = "",
+    val City:String? = "",
+    val State:String? = "",
+    val Images:String? = "",
+
+)
