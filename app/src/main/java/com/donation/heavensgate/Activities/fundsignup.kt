2b49f86960d6b0beb3e6d8a5214e2998dcba761f
aamirbhai2003 <aamirbhai2003@gmail.com>
2023-03-31 00:40:01 +0530

    anonymous sign in

--- app/src/main/java/com/donation/heavensgate/Activities/fundsignup.kt
+++ app/src/main/java/com/donation/heavensgate/Activities/fundsignup.kt
@@ -95,13 +95,13 @@ class fundsignup : AppCompatActivity() {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInAnonymously:success")
                     val user = auth.currentUser
-                    updateUI(user)
+                    updateUI(currentUser)
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInAnonymously:failure", task.exception)
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
-                    updateUI(null)
+                    updateUI(currentUser)
                 }
             }
 
