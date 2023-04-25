89e88cf29d242c3150ec6b527c9231eb61ae75f6
krakendev01 <sahilpatel19189@gmail.com>
2023-04-25 23:32:57 +0530

    ok sir

--- app/src/main/java/com/donation/heavensgate/Activities/fundsignup.kt
+++ app/src/main/java/com/donation/heavensgate/Activities/fundsignup.kt
@@ -214,6 +214,7 @@ class fundsignup : AppCompatActivity() {
 
                     Log.d(ContentValues.TAG, "signInAnonymously:success")
 
+
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(ContentValues.TAG, "signInAnonymously:failure", task.exception)
