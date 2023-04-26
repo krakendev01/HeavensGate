bd35c4a41229d365b48996a1b136b4e069e472b6
krakendev01 <sahilpatel19189@gmail.com>
2023-04-26 16:05:46 +0530

    navigation set up

--- app/src/main/java/com/donation/heavensgate/MainActivity.kt
+++ app/src/main/java/com/donation/heavensgate/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.donation.heavensgate
 
 import android.os.Bundle
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.donation.heavensgate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val naveHostFragment = supportFragmentManager.findFragmentById(R.id.fundfragmentcontainer)
+        val navController = naveHostFragment!!.findNavController()
+
+        val popupMenu= PopupMenu(this, null)
+        popupMenu.inflate(R.menu.fund_bottom_nav)
+        binding.fundbottomBar.setupWithNavController(popupMenu.menu,navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title = when(destination.id){
+                    R.id.FundHomeFragment -> "Home"
+                    else -> "Profile"
+                }
+            }
+
+        })
+
     }
 
     override fun onBackPressed() {
