02b7b0222e6d125e55648c56f02f3e9bd1f30043
PatelShakil <patelsakib95@gmail.com>
2023-04-28 11:22:07 +0530

    Payment Reciept Download Added

--- app/src/main/java/com/donation/heavensgate/adapter/DonatorRecieptAdapter.kt
+++ app/src/main/java/com/donation/heavensgate/adapter/DonatorRecieptAdapter.kt
@@ -1,24 +1,24 @@
 package com.donation.heavensgate.adapter
 
-import android.Manifest
-import android.app.Activity
-import android.content.pm.PackageManager
+import android.content.ContentValues
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
+import android.os.Build
 import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.donation.heavensgate.databinding.SampleDonationListBinding
 import com.donation.heavensgate.models.AddOrgModel
 import com.donation.heavensgate.models.Transaction
 import com.donation.heavensgate.models.User
-
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -28,12 +28,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.itextpdf.text.Document
 import com.itextpdf.text.Image
 import com.itextpdf.text.pdf.PdfWriter
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.FileOutputStream
-import java.text.SimpleDateFormat
+import java.io.*
 import java.util.*
-import kotlin.coroutines.coroutineContext
 
 class DonatorRecieptAdapter(var myTransList: List<Transaction>) :
     RecyclerView.Adapter<DonatorRecieptAdapter.DonatorRecieptViewHolder>() {
@@ -90,10 +86,7 @@ class DonatorRecieptAdapter(var myTransList: List<Transaction>) :
 
         }
         private fun generatePdf(layout: View) {
-
-            val rootDir = Environment.getExternalStorageDirectory()
-            val dir = File(rootDir, "Downloads/Heavans_Gate")
-
+            val dir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Heavans_Gate")
             if (!dir.exists()) {
                 dir.mkdirs()
             }
@@ -108,13 +101,48 @@ class DonatorRecieptAdapter(var myTransList: List<Transaction>) :
                 document.open()
                 val stream = ByteArrayOutputStream()
                 layout.isDrawingCacheEnabled = true
-                Bitmap.createBitmap(layout.drawingCache)
-                    .compress(Bitmap.CompressFormat.PNG, 100, stream)
-                val image = Image.getInstance(stream.toByteArray())
-                document.add(image)
+
+// Measure and layout the view to its regular size
+                val widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(layout.width, View.MeasureSpec.EXACTLY)
+                val heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(layout.height, View.MeasureSpec.EXACTLY)
+                layout.measure(widthMeasureSpec, heightMeasureSpec)
+                layout.layout(0, 0, layout.measuredWidth, layout.measuredHeight)
+
+                // Enable drawing cache before creating bitmap
+                layout.isDrawingCacheEnabled = true
+
+                // Create a bitmap of the entire layout
+                val bitmap = Bitmap.createBitmap(layout.width, layout.height, Bitmap.Config.ARGB_8888)
+                val canvas = Canvas(bitmap)
+                layout.draw(canvas)
+                bitmap.compress(Bitmap.CompressFormat.PNG,100,stream)
+                val img = Image.getInstance(stream.toByteArray())
+                val scaler: Float = (document.pageSize.width - document.leftMargin()
+                        - document.rightMargin() - 0) / img.getWidth() * 100
+                img.scalePercent(scaler)
+                img.setAlignment(Image.ALIGN_CENTER or Image.ALIGN_TOP)
+                document.add(img)
                 document.close()
+                Toast.makeText(layout.context,"Check out Downloads/Heavans_Gate folder",Toast.LENGTH_LONG).show()
+
                 Log.d("PDF","Generated Successfully")
-            }catch (e :Exception){
+
+                // Save the PDF file to the device's media store
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    val resolver = layout.context.contentResolver
+                    val contentValues = ContentValues().apply {
+                        put(MediaStore.MediaColumns.DISPLAY_NAME, file.name)
+                        put(MediaStore.MediaColumns.MIME_TYPE, "application/pdf")
+                        put(MediaStore.MediaColumns.RELATIVE_PATH, "Download/Heavans_Gate")
+                    }
+                    resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+                } else {
+                    val mediaScannerIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+                    mediaScannerIntent.data = Uri.fromFile(pdfFile)
+                        layout.context.sendBroadcast(mediaScannerIntent)
+
+                }
+            } catch (e :Exception){
                 e.printStackTrace()
             }
         }
